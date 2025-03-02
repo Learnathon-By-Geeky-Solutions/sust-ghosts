d92daf1ff6765ed6ecb44c3762e65c31d348d21b
Khalid Bin Selim <131846762+KhalidBinSelim@users.noreply.github.com>
2025-03-02 11:27:53 +0600

    frontpage

---
+++ frontend/src/app.js
@@ -0,0 +1,7 @@
+const moongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');   
+const app = express();
+const port = 3000;
