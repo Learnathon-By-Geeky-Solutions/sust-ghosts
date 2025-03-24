095416d3c5ae8b03f702f8d4224ac9202b245345
Rezaul Karim <rezaul0818@gmail.com>
2025-03-24 16:15:33 +0600

    Reminder Repo

---
+++ backend/src/modules/reminder/reminder.repository.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose');
+const userSchema = require('../user/user.model')
+const User = mongoose.model('User',userSchema);
+const Reminder = mongoose.model(require('/reminder.model'));
+
+
+
+
+
+// CRUD operations for reminder
+const createReminder = async (reminderData) => {
+    const reminder = new Reminder(reminderData);
+    return await reminder.save();
+};
+
+const getAllReminders = async () => {
+    return await Reminder.find().populate('assignee', 'email');
+};
+
+const getReminderById = async (reminderId) => {
+    return await Reminder.findById(reminderId).populate('assignee', 'email');
+};
+
+const updateReminder = async (reminderId, updatedData) => {
+    return await Reminder.findByIdAndUpdate(reminderId, updatedData, { new: true });
+};
+
+const deleteReminder = async (reminderId) => {
+    return await Reminder.findByIdAndDelete(reminderId);
+};
+
+module.exports = {
+    createReminder,
+    getAllReminders,
+    getReminderById,
+    updateReminder,
+    deleteReminder
+};
