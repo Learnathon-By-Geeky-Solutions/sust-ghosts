c3fbfaca15b5f40e1c1323ccca6fcff14cee6768
Rezaul Karim <rezaul0818@gmail.com>
2025-03-24 15:51:49 +0600

    Creating reminder module

---
+++ backend/src/modules/reminder/reminder.model.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const taskSchema = mongoose.Schema({
+    
+    assignee: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        require: true
+    },
+
+    desc: {
+        type: String, 
+        require: true    
+    },
+
+    time: {
+        type: String,
+        require: true
+    },
+    status: {
+        type: String,
+        enum: ['To Do', 'Done', 'Delegated'],
+        require: true
+    },
+})
+module.exports = taskSchema
